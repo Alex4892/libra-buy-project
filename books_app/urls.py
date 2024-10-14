7c753c5fae9a6b6973c132b5a32c075967a5bea3
Alex <Alekseytorshin@gmail.com>
2024-10-14 19:52:42 +0300

    Добавлена главная страница. Добавлен базовый шаблон.

--- books_app/urls.py
+++ books_app/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
 
+from .views import view_books
+
 
 app_name = "books"
 urlpatterns = [
-    path()
+    path('', view_books, name='index')
 ]
--- books_app/views.py
+++ books_app/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
+def view_books(request):
+    return render(request, "books/index.html")
+
 # Create your views here.
